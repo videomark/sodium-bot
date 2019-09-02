9ec9aacddc20c5a775dd1dcc83884538df208b70
Kohei Watanabe <kou029w@gmail.com>
2019-09-02 11:52:32 +0900

    終了に成功したら削除

--- down.js
+++ down.js
@@ -1,14 +1,20 @@
+const { unlink } = require("fs").promises;
 const assert = require("assert");
 const { HttpClient, Executor } = require("selenium-webdriver/http");
 const { Driver } = require("selenium-webdriver/chrome");
 
-const { SELENIUM_REMOTE_URL } = process.env;
-Object.entries({ SELENIUM_REMOTE_URL }).forEach(([env, value]) => {
-  assert(value != null, `${env} required.`);
-});
+const down = async () => {
+  const { SELENIUM_REMOTE_URL } = process.env;
+  Object.entries({ SELENIUM_REMOTE_URL }).forEach(([env, value]) => {
+    assert(value != null, `${env} required.`);
+  });
 
-const client = new HttpClient(SELENIUM_REMOTE_URL);
-const executor = new Executor(client);
+  const client = new HttpClient(SELENIUM_REMOTE_URL);
+  const executor = new Executor(client);
 
-new Driver(require("./session.json"), executor).quit();
-console.log("Quit session.");
+  new Driver(require("./session.json"), executor).quit();
+  await unlink("./session.json");
+  console.log("Quit session.");
+};
+
+down();

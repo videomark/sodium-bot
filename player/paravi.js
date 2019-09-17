ddd100a11a4c3266649d263461a573221f274d0a
Kohei Watanabe <kou029w@gmail.com>
2019-09-17 15:38:45 +0900

    再生ボタンを押す (Paravi)

--- player/paravi.js
+++ player/paravi.js
@@ -1,3 +1,18 @@
+const { WebDriver, By, until } = require("selenium-webdriver");
 const Player = require("./player");
 
-module.exports = { ParaviPlayer: Player };
+class ParaviPlayer extends Player {
+  /**
+   * @override
+   * @param {{driver: WebDriver, url: URL}} options
+   */
+  async play({ driver, url }) {
+    await super.play({ driver, url });
+
+    const playButton = By.css("i.fa.fa-play5");
+    await driver.wait(until.elementLocated(playButton));
+    await driver.findElement(playButton).then(el => el.click());
+  }
+}
+
+module.exports = { ParaviPlayer };
